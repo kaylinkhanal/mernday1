920a33b15f6102a60c4cdff6aa6a1f90db053f5d
kandelarjun67 <kandelarjun67@gmail.com>
2022-11-18 17:26:19 +0545

    reverse the array element

---
+++ reverseArray.js
@@ -0,0 +1,5 @@
+// reverse an array 
+let broadwayMernClass = ['6am', '7am', '8.15am', '2pm']
+let reverseArray = broadwayMernClass.reverse();
+
+console.log(reverseArray)    //  [ '2pm', '8.15am', '7am', '6am' ]
