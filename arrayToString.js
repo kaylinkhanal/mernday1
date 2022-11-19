94035f36669ba65c089d7987eee8db02cbdc4d0c
kandelarjun67 <kandelarjun67@gmail.com>
2022-11-19 22:28:33 +0545

    change array to string

---
+++ arrayToString.js
@@ -0,0 +1,7 @@
+let arr = [1, 8, 11, 16, 31]
+
+// change to string using toString()
+
+let arrResult = arr.toString()  // let arrResult is now string
+
+console.log(arrResult);     // '1,8,11,16,31'
