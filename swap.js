50ac4b1dc3718c01961bc38cc6a9fcca4aded994
mrrk10 <roshan10kandel@gmail.com>
2022-11-22 09:00:54 +0545

    swap without third variable

---
+++ swap.js
@@ -0,0 +1,49 @@
+// function sayHi() {
+//     console.log(name);
+//     console.log(age);
+//     var name = 'Lydia';
+//     let age = 21;
+//   }
+  
+//   sayHi();
+// add(2,3);
+
+// var add=function sum(a,b){
+//     return a+b;
+// }
+
+// for (var i = 0; i < 3; i++) {
+//     setTimeout(() => console.log(i), 1);
+//   }
+  
+//   for (let i = 0; i < 3; i++) {
+//     setTimeout(() => console.log(i), 1);
+//   }
+
+// function detail(){
+//     var name ='rosjan';
+// }
+// detail();
+// console.log(name);
+
+// let c = { greeting: 'Hey!' };
+// let d;
+
+// d = c;
+// c.greeting = 'Hello';
+// console.log(d.greeting);
+
+
+// a=[1,2,3,4]
+// for(var i in a){
+//     console.log()
+// }
+
+var a=1
+var b=2
+a=a+b
+b=a-b
+a=a-b
+
+console.log('a>>>',a)
+console.log('b>>>',b)
