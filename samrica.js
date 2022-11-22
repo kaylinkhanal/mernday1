063bcf670b4afa9ac3d6c4ed4df24a312b443967
Samrica1 <samrica62@gmail.com>
2022-11-22 09:04:55 +0545

    added features

---
+++ samrica.js
@@ -0,0 +1,4 @@
+const arr=[2,2,3,4,2]
+arr.map((item,id)=>{
+    if(item=2)
+    return(item)})
---
+++ surakshyaedit.js
@@ -0,0 +1,6 @@
+const array=[3,2,4,5,6,4]
+console.log(array)
+array.push(53)
+console.log(array)
+const add = array.map(item => item+53)
+console.log(add)
